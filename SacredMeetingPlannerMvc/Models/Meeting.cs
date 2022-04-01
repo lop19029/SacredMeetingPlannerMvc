a62f9a555dfd01334dfea599f630d25980d02a57
Alex López <alexhumbertolds@gmail.com>
2022-04-01 15:01:35 -0300

    Refactor Person class

--- SacredMeetingPlannerMvc/Models/Meeting.cs
+++ SacredMeetingPlannerMvc/Models/Meeting.cs
@@ -7,12 +7,12 @@ namespace SacredMeetingPlannerMvc.Models
     {
         public int ID { get; set; }
         public DateTime Date { get; set; }
-        public Leader Leader { get; set; }
+        public Person Leader { get; set; }
         public int OpeningSong { get; set; }
         public int SacramentSong { get; set; }
         public int IntermediateSong { get; set; } //optional
         public int ClosingSong { get; set; }
-        public List<Speaker> Speakers { get; set; }
+        public List<Person> Speakers { get; set; }
 
     }
 }
--- SacredMeetingPlannerMvc/Models/Person.cs
+++ SacredMeetingPlannerMvc/Models/Person.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 
 namespace SacredMeetingPlannerMvc.Models
 {
-    public abstract class Person
+    public class Person
     {
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
-       
+        public string IsLeader { get; set; }
+        public string Subject { get; set; }
     }
 }
