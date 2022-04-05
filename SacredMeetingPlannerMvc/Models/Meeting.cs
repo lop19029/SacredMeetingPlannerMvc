98ee5c7b0415748f1c06c336225c3da3616c57a9
juliano1408 <bbfurlong@gmail.com>
2022-04-05 18:21:19 -0400

    Data Validation

--- SacredMeetingPlannerMvc/Models/Meeting.cs
+++ SacredMeetingPlannerMvc/Models/Meeting.cs
@@ -8,19 +8,27 @@ namespace SacredMeetingPlannerMvc.Models
     public class Meeting
     {
         public int ID { get; set; }
+
         public DateTime Date { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         public Person Leader { get; set; }
+
         [Required]
         [DisplayName("Opening Hymn")]
         public int OpeningSong { get; set; }
+
         [Required]
         [DisplayName("Sacrament Hymn")]
         public int SacramentSong { get; set; }
+
         [DisplayName("Intermediate Hymn")]
         public string IntermediateSong { get; set; } //set to string to allow for a nullable property. Ints cannot be null -Harrison
+
         [Required]
         [DisplayName("Closing Hymn")]
         public int ClosingSong { get; set; }
+
         public List<Person> Speakers { get; set; }
 
     }
--- SacredMeetingPlannerMvc/Models/Person.cs
+++ SacredMeetingPlannerMvc/Models/Person.cs
@@ -9,14 +9,22 @@ namespace SacredMeetingPlannerMvc.Models
     public class Person
     {
         public int ID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         [Required]
         [DisplayName("Last Name")]
         public string LastName { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         [Required]
         [DisplayName("First Name")]
         public string FirstName { get; set; }
+
+        
         [DisplayName("Leader")]
         public bool IsLeader { get; set; }
+
+        [StringLength(100, MinimumLength = 8)]
         [Required]
         public string Subject { get; set; }
   
